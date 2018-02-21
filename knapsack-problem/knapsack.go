4a81ad7d5975b5cfd1066863e5f3e0bf0725097b
Rylan <rylans@gmail.com>
2018-02-21 11:44:51 -0800

    Add example with 10 items.

--- knapsack-problem/knapsack.go
+++ knapsack-problem/knapsack.go
@@ -16,38 +16,20 @@ func hasItemN(byte uint8, n int) bool {
 	return (byte & mask) > 0
 }
 
-func main() {
-	fmt.Println("Knapsack problem")
-
-	b := chromosomes.NewBuilder()
-	b.AddTrait("A")
-
-	maxWt := 10
-	item0 := knapsackItem{5, 10}
-	item1 := knapsackItem{4, 40}
-	item2 := knapsackItem{6, 30}
-	item3 := knapsackItem{3, 50}
-
-	f := func(c *chromosomes.Chromosome) float64 {
-		totalWt := 0
-		totalVal := 0
+func itemFitnessFn(maxWt int, items []knapsackItem) func(c *chromosomes.Chromosome) float64 {
+	fitnessFn := func(c *chromosomes.Chromosome) float64 {
+		totalWt, totalVal := 0, 0
 		byte := c.Get("A")
+		byte2 := c.Get("B")
 
-		if hasItemN(byte, 0) {
-			totalVal += item0.value
-			totalWt += item0.weight
-		}
-		if hasItemN(byte, 1) {
-			totalVal += item1.value
-			totalWt += item1.weight
-		}
-		if hasItemN(byte, 2) {
-			totalVal += item2.value
-			totalWt += item2.weight
-		}
-		if hasItemN(byte, 3) {
-			totalVal += item3.value
-			totalWt += item3.weight
+		for i := 0; i < len(items); i++ {
+			if i > 8 {
+				byte = byte2
+			}
+			if hasItemN(byte, i) {
+				totalVal += items[i%8].value
+				totalWt += items[i%8].weight
+			}
 		}
 
 		if totalWt <= maxWt {
@@ -57,20 +39,77 @@ func main() {
 		}
 	}
 
+	return fitnessFn
+}
+
+func knapsack4() {
+	fmt.Println("Knapsack problem with four items. Max weight: 10")
+
+	b := chromosomes.NewBuilder()
+	b.AddTrait("A")
+	b.AddTrait("B")
+
+	maxWt := 10
+	items := make([]knapsackItem, 0)
+
+	items = append(items, knapsackItem{5, 10})
+	items = append(items, knapsackItem{4, 40})
+	items = append(items, knapsackItem{6, 30})
+	items = append(items, knapsackItem{3, 50})
+
+	f := itemFitnessFn(maxWt, items)
 	result := optimize.Optimize(f, b)
-	fmt.Println(result, f(result))
+	fmt.Println("Result:", result, f(result)) // Best value: 90
 
 	byte := result.Get("A")
-	if hasItemN(byte, 0) {
-		fmt.Println("contains item0", item0)
+	for i := 0; i < len(items); i++ {
+		if hasItemN(byte, i) {
+			fmt.Println("Contains item #", i, items[i])
+		}
 	}
-	if hasItemN(byte, 1) {
-		fmt.Println("contains item1", item1)
+}
+
+// Example comes from
+// G. Zäpfel et al. Metaheuristic Search Concepts: A Tutorial with Applications to Production and Logistics
+func knapsack10() {
+	fmt.Println("Knapsack problem with ten items. Max weight: 113")
+
+	b := chromosomes.NewBuilder()
+	b.AddTrait("A")
+	b.AddTrait("B")
+
+	maxWt := 113
+	items := make([]knapsackItem, 0)
+
+	items = append(items, knapsackItem{32, 727})
+	items = append(items, knapsackItem{40, 763})
+	items = append(items, knapsackItem{44, 60})
+	items = append(items, knapsackItem{20, 606})
+	items = append(items, knapsackItem{1, 45})
+	items = append(items, knapsackItem{29, 370})
+	items = append(items, knapsackItem{3, 414})
+	items = append(items, knapsackItem{13, 880})
+	items = append(items, knapsackItem{6, 133})
+	items = append(items, knapsackItem{39, 820})
+
+	f := itemFitnessFn(maxWt, items)
+	result := optimize.Optimize(f, b)
+	fmt.Println("Result:", result, f(result)) // Best value: 3580
+
+	byte := result.Get("A")
+	byte2 := result.Get("B")
+	for i := 0; i < len(items); i++ {
+		if i > 8 {
+			byte = byte2
+		}
+		if hasItemN(byte, i) {
+			fmt.Println("Contains item #", i, items[i%8])
+		}
 	}
-	if hasItemN(byte, 2) {
-		fmt.Println("contains item2", item2)
-	}
-	if hasItemN(byte, 3) {
-		fmt.Println("contains item3", item3)
-	}
+}
+
+func main() {
+	knapsack4()
+	fmt.Println()
+	knapsack10()
 }
